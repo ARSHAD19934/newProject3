5c6d14fbc683d4cb4ba4cd5490df114efba739cc
ARSHAD19934 <arshadayyubbaghwan@gmail.com>
2023-08-20 19:26:47 +0530

    demo 2 class updated with one new script related to payment

--- src/test/java/integration/Demo.java
+++ src/test/java/integration/Demo.java
@@ -1,14 +1,23 @@
 package integration;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Demo {
 	
-	@Test
+	@Test(groups ="regression")
 	
 	public void createDemo()
 	{
-		System.out.println("done");
+		//System.out.println("done");
+		Reporter.log("good",true);
+	}
+	
+	@Test
+	public void createDemo1()
+	{
+		//System.out.println("done");
+		Reporter.log("Very Good ",true);
 	}
 
 }
